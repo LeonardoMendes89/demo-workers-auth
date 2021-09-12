0ae26da0b90ffc597479ea43d831d3f69c1a4ea2
Leozin89python <leozin069@gmail.com>
2021-09-12 04:42:17 -0300

    demo

--- routes/auth.js
+++ routes/auth.js
@@ -5,14 +5,15 @@ const authRouter = express()
 
 authRouter.route('/').post(async(req,res)=>{
     
-
-        userlogin = req.body.userlogin,
-        passlogin = req.body.passlogin
+        let data = {
+            userlogin : req.body.userlogin,
+            passlogin : req.body.passlogin
+        }
   
   
 
    await knex.select(['id','userlogin'])
-                .whereRaw(`${ userlogin } = userlogin AND ${passlogin} = passlogin`)  
+                .whereRaw(`${ data.userlogin } = userlogin AND ${data.passlogin} = passlogin`)  
                 .table('auth')
                 .then(_ =>res.status(200).json({msg:"usuário logado com sucesso!"}))
                 .catch(err => res.status(500).json(err))
