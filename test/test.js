e29a216adb88e4c3613f2338770c1c1903c4b4b9
Leozin89python <leozin069@gmail.com>
2021-09-12 03:35:14 -0300

    demo

---
+++ test/test.js
@@ -0,0 +1,17 @@
+const dev   = process.env.NODE_ENV      ||'development' 
+const prod  = process.env.DATABASE_URL  ||'production' 
+const db    = require('../knexfile')[prod] 
+const knex  = require('knex')(db)
+
+const express    = require('express')
+const testRouter = express()
+
+testRouter.route('/').get(async(req,res)=>{
+    await knex.select('*').table('auth')
+                          .then(account => res.status(200).json(account))
+                          .catch(err    => res.status(500).json(err))
+}).post(async(req,res)=>{
+
+})
+
+module.exports = testRouter 
