e3aacb24a25e53dfd89d5acca41067c9c69bb81a
Leozin89python <leozin069@gmail.com>
2021-09-12 15:33:57 -0300

    demo

--- test/test.js
+++ test/test.js
@@ -40,7 +40,12 @@ testRouter.route('/where').get(async(req,res)=>{
 
    const { userlogin, passlogin } = req.body
 
-    await knex.select('*')
+    if(     userlogin != '' || userlogin != null
+        &&
+            passlogin != '' || passlogin != null
+     ){
+
+        return await knex.select('*')
                          .where('userlogin',userlogin)
                          .andWhere('passlogin', passlogin)
                          .table('auth')
@@ -49,7 +54,11 @@ testRouter.route('/where').get(async(req,res)=>{
                                 return res.status(200).json(e)
                              })
                          })
-                         .catch(err => res.status(500).json(err))
+                         .catch(err => res.status(404).json(err))
+
+    }else{
+        return res.status(400).json({msg:'os campos devem ser diferentes de vazio!'})
+    }
 
 })
 
